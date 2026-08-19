4a4fbf6c3b05ff5327d50042e7634935a127dcca
Eric Niemeyer <eric.niemeyer@venturetec.net>
2026-08-19 13:30:11 -0400

    ok

--- src/components/battlePresets.ts
+++ src/components/battlePresets.ts
@@ -176,7 +176,7 @@ function buildPresets(): readonly BattlePreset[] {
       converterTax: 0.5,
       centerMagnitude: 200,
       dividersMagnitude: -3200,
-      perimeterMagnitude: -800,
+      perimeterMagnitude: -400,
       terrainDTerrain: 0,
       plateauWallSlopeDegrees: 89,
       metalDepositStep: 200,
